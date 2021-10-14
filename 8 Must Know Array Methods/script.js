4e552cf723c4268e7c605ca6daa4a94a47360b6c
noorqidam <noorqidam@gmail.com>
2021-10-14 10:36:52 +0700

    8 Must Know Array Methods

---
+++ 8 Must Know Array Methods/script.js
@@ -0,0 +1,49 @@
+const items = [
+  { name: "Bike", price: 100 },
+  { name: "TV", price: 200 },
+  { name: "Album", price: 10 },
+  { name: "Book", price: 5 },
+  { name: "Phone", price: 500 },
+  { name: "Computer", price: 1000 },
+  { name: "Keyboard", price: 25 },
+];
+
+const filteredItems = items.filter((item) => {
+  return item.price <= 100;
+});
+
+console.log(items);
+console.log(filteredItems);
+
+const itemNames = items.map((item) => {
+  return item.name;
+});
+
+console.log(itemNames);
+
+const foundItem = items.find((item) => {
+  return item.name === "Book";
+});
+
+console.log(foundItem);
+
+items.forEach((item) => {
+  console.log(item.name);
+});
+
+const hasInexpensiveItems = items.every((item) => {
+  // some, every
+  return item.price <= 100;
+});
+
+console.log(hasInexpensiveItems);
+
+const total = items.reduce((currentTotal, item) => {
+  return item.price + currentTotal;
+}, 0);
+
+console.log(total);
+
+const search = [1, 2, 3, 4, 5];
+const incledesTwo = search.includes(2);
+console.log(incledesTwo);
