0e44da577a66b954e2e722016d0c683009b738b9
noorqidam <noorqidam@gmail.com>
2023-10-30 22:05:08 +0700

    feat: 🎨 function reverse string

---
+++ Test Case Native Qustion/reverseString.js
@@ -0,0 +1,8 @@
+let string = "javascript";
+let reverseString = "";
+
+for (let index = string.length - 1; index >= 0; index--) {
+  reverseString += string[index];
+}
+
+console.log(reverseString);
